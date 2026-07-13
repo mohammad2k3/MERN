ede15c5cc53defdb8cb7e7f8d9f59b22511fdf29
mohammad2k3 <ahmadraza97x@gmail.com>
2026-07-13 23:14:36 +0530

    java script

---
+++ JAVASCRIPT/2_Variable.js
@@ -0,0 +1,17 @@
+// let
+// var FUNCTION SCOPED
+// const
+
+
+// var age = 25;
+// if(true)
+// {
+//     console.log(age);
+// }
+
+function solve()
+{
+    var age = 25;
+    console.log(age);
+}
+solve();
